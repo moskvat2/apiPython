d3b5b75bda0d0a79028a9a35ea4fc2a8a0e79e53
Gerson <moskvatrussian@gmail.com>
2022-12-04 17:25:05 -0300

    criar tabelas

--- geekuniversity/secao04/criar_tabelas.py
+++ geekuniversity/secao04/criar_tabelas.py
@@ -9,10 +9,11 @@ async def create_table() -> None:
     async with engine.begin() as conn:
         await conn.run_sync(settings.DBBaseModel.metadata.drop_all)
         await conn.run_sync(settings.DBBaseModel.metadata.create_all)
+        
     print('Tabelas criadas com sucesso.')
 
 
 
 if __name__ == '__main__':
     import asyncio
-    asyncio.run(create_table)
+    asyncio.run(create_table())

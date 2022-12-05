cbf36d46bbbe653924aa77977a32a53378ab0ff7
Gerson <moskvatrussian@gmail.com>
2022-12-05 20:53:14 -0300

    agora vai

---
+++ geekuniversity/secao06/api/v1/api.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+from api.v1.endpoints import artigo
+from api.v1.endpoints import usuario
+
+
+api_route = APIRouter()
+
+
+api_route.include_router(artigo.router, prefix='/artigos', tags=['artigos'])
+api_route.include_router(artigo.router, prefix='/usuarios', tags=['usuarios'])
